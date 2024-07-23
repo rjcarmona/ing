01c948d36f40fbf53429b605f9612250c9d3c5e8
RUben <rubencarmonaparedes@gmail.com>
2024-07-23 20:40:37 +0200

    Add Lit example

--- dropdown/dropdown.js
+++ dropdown/dropdown.js
@@ -4,3 +4,6 @@ const wrapper = document.querySelector(".wrapper"),
   options = wrapper.querySelector(".options");
 
 selectBtn.addEventListener("click", () => wrapper.classList.toggle("active"));
+
+// For the <li> tag you could do something like this on Lit
+// this.igcValues?.map(val => html`<li>${val}</li>`);
